c48c6365eaa4ffa8efbc8f9dc6a8c4a8515b5d35
macchi-ato <macchikay@gmail.com>
2026-01-07 11:04:06 -0500

    Home intro layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,8 +8,18 @@ export default function Home() {
             </aside>
 
             <main className="home-content">
-                <section id="intro-paragraph">Intro Paragraph</section>
+                <section className="home-intro">
+                    <div className="intro-container">
+                        <h1>Hello! I'm Name</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis accusantium consectetur veniam, at mollitia repellat placeat reiciendis corrupti, odit itaque voluptate, facere esse minima sequi cum ab ut nulla fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas error temporibus atque deleniti ad illo ea, quibusdam suscipit dolor, sequi nisi autem et quidem, facere impedit. Nesciunt reprehenderit impedit nostrum.
+                        </p>
+                    </div>
+                    <span>More about me {`->`}</span>
+                </section>
 
+                <h2>Recent Projects</h2>
                 <section className="home-projects">
                     <div>Project 1</div>
                     <div>Project 2</div>
