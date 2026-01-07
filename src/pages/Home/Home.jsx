95872ef7a9121a8b49ef3c9da04fdc1e2b8c71e2
macchi-ato <macchikay@gmail.com>
2026-01-07 14:12:40 -0500

    Color pallette variables

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,13 +10,13 @@ export default function Home() {
             <main className="home-content">
                 <section className="home-intro">
                     <div className="intro-container">
-                        <h1>Hello! I'm Name</h1>
+                        <h1>Hello! I'm <span id="intro-name">Name</span></h1>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis accusantium consectetur veniam, at mollitia repellat placeat reiciendis corrupti, odit itaque voluptate, facere esse minima sequi cum ab ut nulla fugiat.
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas error temporibus atque deleniti ad illo ea, quibusdam suscipit dolor, sequi nisi autem et quidem, facere impedit. Nesciunt reprehenderit impedit nostrum.
                         </p>
                     </div>
-                    <span>More about me {`->`}</span>
+                    <span>More {`->`}</span>
                 </section>
 
                 <h2>Recent Projects</h2>
