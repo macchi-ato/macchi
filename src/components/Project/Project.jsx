985395a19cc9b659faaacaf74d801ab7a78719c0
macchi-ato <macchikay@gmail.com>
2026-01-14 15:54:00 -0500

    Project card restructuring

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,9 +1,15 @@
 import './Project.css'
+import projectImg from '../../assets/shanks.jpg'
 
 export default function Project({ title }) {
     return (
         <div className="project-card">
-            <h3>{title}</h3>
+            <img src={projectImg} alt="Project" id="project-img" />
+
+            <div className="project-description">
+                <h3 id="project-title">{title}</h3>
+                <p id="project-about">This is a just a testing description.</p>
+            </div>
         </div>
     )
 }
