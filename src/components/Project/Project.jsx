d6feb3e420ee980fbe26dcebf4fea7e01c5d56c7
macchi-ato <macchikay@gmail.com>
2026-01-15 15:43:37 -0500

    Style changes

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -5,12 +5,15 @@ export default function Project({ title, description, language }) {
 
     return (
         <div className="project-card">
-            <img src={projectImg} alt={title || 'Project'} id="project-img" />
+            <img src={projectImg} alt={title || "Project"} id="project-img" />
 
-            <div className="project-description">
+            <div className="project-details">
                 <h3 id="project-title">{title}</h3>
-                <p id="project-about">{description || 'No description available.'}</p>
-                {language && <span id="project-language">{language}</span>}
+
+                <div className="project-description">
+                    <p id="project-about">{description || "No description available."}</p>
+                    {language && <span id="project-language">{language}</span>}
+                </div>
             </div>
         </div>
     )
