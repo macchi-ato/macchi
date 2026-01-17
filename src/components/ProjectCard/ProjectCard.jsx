74dbe86e823b435f7e159655f35b94dd38afa9f0
macchi-ato <macchikay@gmail.com>
2026-01-17 14:07:50 -0500

    Routing to project page

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,20 +1,23 @@
 import './ProjectCard.css'
 import projectImg from '../../assets/theChip.jpg'
+import { Link } from "react-router-dom"
 
 export default function Project({ title, description, language }) {
 
     return (
-        <div className="project-card">
-            <img src={projectImg} alt={title || "Project"} id="project-img" />
+        <Link to={`/projects/${title}`} id="link-to-project">
+            <div className="project-card">
+                <img src={projectImg} alt={title || "Project"} id="project-img" />
 
-            <div className="project-details">
-                <h3 id="project-title">{title}</h3>
+                <div className="project-details">
+                    <h3 id="project-title">{title}</h3>
 
-                <div className="project-description">
-                    <p id="project-about">{description || "No description available."}</p>
-                    {language && <span id="project-language">{language}</span>}
+                    <div className="project-description">
+                        <p id="project-about">{description || "No description available."}</p>
+                        {language && <span id="project-language">{language}</span>}
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
