5c4af5045be2d662c35b04ffed145a8d6efa9d81
macchi-ato <macchikay@gmail.com>
2026-01-22 14:37:52 -0500

    fix: typo

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -16,7 +16,7 @@ export default function About() {
 
                 <p>My name is Martin Vidal Garibay and I’m a recent Software Engineering graduate with a focus on full-stack development.</p>
                 <p>Right now, my main focus is landing a full-time software engineering role while continuing to grow as a developer. I’m open to different areas of software, but I’ve been slowly leaning more toward backend and systems-focused work. Most of my time is spent building projects, strengthening fundamentals, and learning more about how software behaves at a lower level. Alongside this, I’m continuing my education through Georgia Tech’s OMSCS program starting in Fall 2026, where I plan to deepen my understanding of computing systems while keeping my primary focus on industry work.</p>
-                <p>Besides software, I like doing things like playing pickleball, going on walks with my dog, and sometimes doing 3D modeling in Blender. Some of my guilty pleasures are (unfortunately) playing League of Legends or watching K-dramas.</p>
+                <p>Besides software, I like doing things like playing pickleball, going on walks with my dog, and sometimes doing 3D modeling in Blender. Some of my guilty pleasures are (unfortunately) playing League of Legends and watching K-dramas.</p>
                 <p>Thanks for stopping by — feel free to reach out :)</p>
             </main>
         </div>
