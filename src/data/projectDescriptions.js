d50da9a43a2ab848f5d4f67e2e8e2ea1051d4386
macchi-ato <macchikay@gmail.com>
2026-01-23 10:13:46 -0500

    added metadata template for project descriptions

---
+++ src/data/projectDescriptions.js
@@ -0,0 +1,12 @@
+/**
+ * Project descriptions metadata
+ * 
+ * Structure:
+ * {
+ *   "repository-name": ["paragraph 1", "paragraph 2", ...],
+ * }
+ */
+
+const projectDescriptions = {}
+
+export default projectDescriptions
