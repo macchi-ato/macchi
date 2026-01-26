ffcf60e3dc6c69c0afb100105e647059611ded4d
macchi-ato <macchikay@gmail.com>
2026-01-26 13:28:39 -0500

    style: profile icons adjustments

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -10,17 +10,15 @@ export default function ProfileCard() {
 
             <div className="profile-links">
                 <a className="profile-link" href="https://github.com/macchi-ato" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
-                    <VscGithubAlt size={24} aria-hidden="true" />
-                    Github
+                    <VscGithubAlt size={28} aria-hidden="true" />
                 </a>
 
                 <a className="profile-link" href="https://www.linkedin.com/in/martin-vidal-garibay-2b912929a/" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer">
-                    <CiLinkedin size={28} aria-hidden="true" />
-                    LinkedIn
+                    <CiLinkedin size={32} aria-hidden="true" />
                 </a>
 
                 <a className="profile-link" href="mailto:vidalgmartin@hotmail.com" aria-label="Email">
-                    <CiMail size={28} aria-hidden="true" />
+                    <CiMail size={32} aria-hidden="true" />
                 </a>
             </div>
         </aside>
