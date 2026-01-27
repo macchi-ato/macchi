5c4fef7a04371a3850cd8a155cd80037722838b2
macchi-ato <macchikay@gmail.com>
2026-01-27 16:26:58 -0500

    style: structure changes

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,5 +1,4 @@
 import "./ProjectCard.css"
-import projectImg from "../../assets/theChip.jpg"
 import { Link } from "react-router-dom"
 import { GoTag } from "react-icons/go"
 import { getLanguageColor } from "../../utils/languageColors"
@@ -11,32 +10,27 @@ export default function Project({ title, description, language, languages, disab
     return (
         <Link to={`/projects/${title}`} id="link-to-project">
             <div className={`project-card ${disableHover ? 'no-hover' : ''}`}>
-                <img src={projectImg} alt={title || "Project"} id="project-img" />
-
-                <div className="project-details">
-                    <h3 id="project-title">{title}</h3>
-
-                    <div className="project-description">
-                        <p id="project-about">{description || "No description available."}</p>
-                        
-                        {displayLanguages.length > 0 && 
-                            <div className="language-container">
-                                <GoTag size={20} aria-hidden="true" />
-                                <div className="languages-list">
-                                    {displayLanguages.map((lang, index) => (
-                                        <span 
-                                            key={index}
-                                            id="project-language"
-                                            style={{ backgroundColor: getLanguageColor(lang) }}
-                                        >
-                                            {lang}
-                                        </span>
-                                    ))}
-                                </div>
-                            </div>
-                        }
-                    </div>
+                <div className="project-img">
+                    <span id="project-title">{title}</span>
+                    <p id="project-about">{description || "No description available."}</p>
                 </div>
+
+                {displayLanguages.length > 0 && 
+                    <div className="language-container">
+                        <GoTag size={20} aria-hidden="true" />
+                        <div className="languages-list">
+                            {displayLanguages.map((lang, index) => (
+                                <span 
+                                    key={index}
+                                    id="project-language"
+                                    style={{ backgroundColor: getLanguageColor(lang) }}
+                                >
+                                    {lang}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+                }
             </div>
         </Link>
     )
