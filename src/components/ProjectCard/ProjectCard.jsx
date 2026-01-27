09f293672207e609645ae4c625c35e458adfbda0
macchi-ato <macchikay@gmail.com>
2026-01-27 18:56:12 -0500

    style: structure change (again)

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import "./ProjectCard.css"
 import { Link } from "react-router-dom"
-import { GoTag } from "react-icons/go"
+import { GoTag, GoRepo } from "react-icons/go"
 import { getLanguageColor } from "../../utils/languageColors"
 
 export default function Project({ title, description, language, languages, disableHover }) {
@@ -10,8 +10,12 @@ export default function Project({ title, description, language, languages, disab
     return (
         <Link to={`/projects/${title}`} id="link-to-project">
             <div className={`project-card ${disableHover ? 'no-hover' : ''}`}>
-                <div className="project-img">
-                    <span id="project-title">{title}</span>
+                <div className="project-details">
+                    <div className="project-header">
+                        <GoRepo size={32} aria-hidden="true" />
+                        <h2>{title}</h2>
+                    </div>
+
                     <p id="project-about">{description || "No description available."}</p>
                 </div>
 
