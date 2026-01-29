499a6d9711e63790be86af372b549945564c258f
macchi-ato <macchikay@gmail.com>
2026-01-29 16:38:31 -0500

    style: removed profile img

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -6,7 +6,8 @@ import { VscGithubAlt } from "react-icons/vsc"
 export default function ProfileCard() {
     return (
         <aside className="profile-card">
-            <img src={profileImg} alt="Profile" className="profile-image" />
+            
+            <div className="profile-image"></div>
 
             <div className="profile-links">
                 <a className="profile-link" href="https://github.com/macchi-ato" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
