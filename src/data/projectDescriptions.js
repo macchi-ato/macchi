d6aa0625ce4ea5ecc83927ab7ac16aec528d8f72
macchi-ato <macchikay@gmail.com>
2026-02-02 10:48:06 -0500

    added macchi project description

--- src/data/projectDescriptions.js
+++ src/data/projectDescriptions.js
@@ -12,6 +12,13 @@ const projectDescriptions = {
         "I use arch btw!",
         "These dotfiles are heavily based on someone else's configuration as I pointed out on github. However, I have made enough changes that I finally feel comfortable calling it my own.",
         "There is nothing impressive here or much to say about it, I even considered omitting this repo, but to this day, tweaking my system is still something I very much enjoy, so I'm happy to include it as one of my projects."
+    ],
+    "macchi" : [
+        "My first porfolio website.",
+        "My goal was for a minimal and clean website to introduce myself, share my current projects, and my resume.",
+        "As a developer, having some sort of portfolio was long overdue. This isn't my first attempt, but it's the first time I've been satisfied with the result to host and share it.",
+        "The biggest challenge was using GitHub's API to dynamically render my projects because I knew that I didn't want to manually have to update my projects, although now I feel like I've just created a second GitHub profile, but with my own style.",
+        "Regardless, I am happy with the result but it's far from being done. I plan on continuing to update it until I run out of ideas or I come up with a new design for its second iteration."
     ]
 }
 
