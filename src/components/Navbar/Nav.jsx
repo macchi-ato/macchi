a8f45c0efe4980cde641c2c574bcdbe9f0824653
macchi-ato <macchikay@gmail.com>
2026-02-02 15:45:49 -0500

    refactor: removed time function and imports

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,35 +1,13 @@
-import { useState, useEffect } from "react"
 import { Link, useLocation } from "react-router-dom"
 import "./Nav.css"
 
 export default function Nav() {
-    const [currentTime, setCurrentTime] = useState("")
     const location = useLocation()
     
     const getCurrentPath = () => {
         const path = location.pathname === "/" ? "" : location.pathname.slice(1)
         return path
     }
-
-    const getCurrentTime = () => {
-        let now = new Date()
-        let hours = now.getHours()
-        let minutes = now.getMinutes()
-        let ampm = hours >= 12 ? 'pm' : 'am'
-        
-        hours = hours % 12
-        hours = hours ? hours : 12 // 0 should be 12
-        
-        const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}${ampm}`
-        setCurrentTime(formattedTime)
-    }
-
-    useEffect(() => {
-        getCurrentTime() // First call
-
-        const interval = setInterval(getCurrentTime, 60000) // Update every minute
-        return () => clearInterval(interval)
-    }, [])
     
     return (
         <nav className="nav-container">
