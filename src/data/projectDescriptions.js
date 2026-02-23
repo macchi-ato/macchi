affa137956c1c4396e1a182805bc304e83630509
macchi-ato <macchikay@gmail.com>
2026-02-23 10:42:40 -0500

    revised project description

--- src/data/projectDescriptions.js
+++ src/data/projectDescriptions.js
@@ -16,11 +16,12 @@ const projectDescriptions = {
         "I considered omitting this repo, but to this day, tweaking my system is still something I very much enjoy, so I'm happy to include it as one of my projects."
     ],
     "macchi" : [
-        "My first porfolio website.",
-        "My goal was for a minimal and clean website to introduce myself, share my current projects, and my resume.",
-        "As a developer, having some sort of portfolio was long overdue. This isn't my first attempt, but it's the first time I've been satisfied with the result to host and share it.",
-        "The biggest challenge was using GitHub's API to dynamically render my projects because I knew that I didn't want to manually have to update my projects, although now I feel like I've just created a second GitHub profile, but with my own style.",
-        "Regardless, I am happy with the result but it's far from being done. I plan on continuing to update it until I run out of ideas or I come up with a new design for its second iteration."
+        "My first portfolio website.",
+        "The goal of this site is to introduce myself, share my projects, and my resume.",
+        "As a developer, having some sort of portfolio was long overdue. This isn't my first attempt, but it's the first time I've been satisfied enough to actually host and share it.",
+        "The biggest challenge was integrating GitHub's API to dynamically pull in my projects. I knew from the start that I didn't want to manually add or update project every time something changed. That being said, this really wasn't a complicated project.",
+        "I built it with React and Vite, keeping it simple to match the simplicity of the site itself. No unnecessary libraries or frameworks, just enough to get the job done.",
+        "I'm happy with the result but it's far from being done. I plan on continuing to update it until I run out of ideas or I come up with a new design for its second iteration."
     ],
     "torus" : [
         "My undergrad capstone project.",
